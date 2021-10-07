265e401513abb90043945d95498805bf0985505a
Víctor Grajeda <vgrajanda@gmail.com>
2021-10-07 12:27:45 -0500

    Replacing all qtile folder with a more simple configuration

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
+xrandr --output Virtual-1 --primary --mode 1440x900 --pos 0x0 --rotate normal --output Virtual-2 --off --output Virtual-3 --off --output Virtual-4 --off --output Virtual-5 --off --output Virtual-6 --off --output Virtual-7 --off --output Virtual-8 --off
+
 nitrogen --restore &
-picom --no-vsync &
-xcompmgr &
--- qtile/config.py
+++ qtile/config.py
@@ -1,32 +1,8 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-from typing import List  # noqa: F401
+# vgrajanda qtile config file
 
 import os, subprocess
+
+from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
@@ -80,8 +56,13 @@ keys = [
 
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawn("rofi -show run"),
-        desc="Run Rofi on run mode"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+
+    # Custom keybinds
+    Key([mod], "b", lazy.spawn("firefox"), desc="Launch web browser"),
+    Key([mod], "e", lazy.spawn("pcmanfm"), desc="Launch file manager"),
+    Key([mod], "r", lazy.spawn("rofi -show run"), desc="Run application")
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -101,51 +82,33 @@ for i in groups:
         #     desc="move focused window to group {}".format(i.name)),
     ])
 
-
-layout_theme = {
-    "border_width": 2,
-    "margin": 12,
-    "border_focus": "#b48ead",
-    "border_normal": "#3b4252"
-}
-
-group_names = [
-    (" 爵  ", {'layout': 'monadtall'}),
-    ("   ", {'layout': 'monadtall'}),
-    ("   ", {'layout': 'monadtall'}),
-    ("   ", {'layout': 'monadtall'}),
-    ("   ", {'layout': 'monadtall'}),
-    ("   ", {'layout': 'monadtall'})
-]
-
-groups = [Group(name, **kwargs) for name, kwargs in group_names]
-
-for i, (name, kwargs) in enumerate(group_names, 1):
-    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))
-    keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name)))
+layout_theme = { "border_width": 3,
+                 "margin": 12,
+                 "border_focus": "b48ead",
+                 "border_normal": "2e3440"
+               }
 
 layouts = [
-    # layout.Columns(border_focus_stack='#d75f5f'),
+    #layout.Columns(border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=4),
+    layout.MonadTall(**layout_theme),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2, **layout_theme),
+    # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    layout.MonadTall(**layout_theme),
-    # layout.MonadWide(**layout_theme)
+    # layout.MonadWide(),
     # layout.RatioTile(),
-    # layout.Tile(**layout_theme)
-    # layout.TreeTab(**layout_theme)
+    # layout.Tile(),
+    # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
 
 widget_defaults = dict(
     font='sans',
-    fontsize=11,
-    padding=4,
+    fontsize=12,
+    padding=3,
 )
-
 extension_defaults = widget_defaults.copy()
 
 screens = [
@@ -153,61 +116,30 @@ screens = [
         top=bar.Bar(
             [
                 widget.GroupBox(
-                    font="Inconsolata Nerd Font",
-                    fontsize=14,
-                    active="#81a1c1",
-                    inactive="#d8dee9",
-                    this_current_screen_border="#4c566a",
-                    highlight_method="block",
+                    font='JetBrainsMono Nerd Font',
+                    fontsize=11,
+                    this_current_screen_border='5e81ac',
+                    active='eceff4',
+                    inactive='4c566a',
+                    highlight_method='block',
+                    borderwidth=2,
+                    padding=3,
+                    margin=3,
+                    use_mouse_wheel=False,
                     disable_drag=True
                 ),
-
                 widget.Spacer(),
-
-                widget.Net(
-                    format='{down} ↓↑ {up}',
-                    font="Inconsolata Nerd Font",
-                    markup=False,
-                    fontsize=14,
-                    foreground='#a3be8c'
-                ),
-
-                widget.Sep(
-                    size_percent=15,
-                    linewidth=3,
-                    padding=10
-                ),
-
-                widget.CPU(
-                    font="Inconsolata Nerd Font",
-                    fontsize=14,
-                    foreground='#d08770'
-                ),
-
-                widget.Sep(
-                    size_percent=15,
-                    linewidth=3,
-                    padding=10
-                ),
-
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-
+                widget.Sep(),
+                widget.CPU(),
+                widget.Sep(),
+                widget.Systray(),
+                widget.Sep(),
                 widget.Clock(
-                    font="Inconsolata Nerd Font",
-                    fontsize=14,
-                    foreground='#ebcb8b',
-                    format='%a %Y-%m-%d %I:%M %p'
+                    format='%Y-%m-%d %a %I:%M %p',
                 ),
             ],
-            22,
-            background='#2e3440',
-            margin=5,
-            opacity=1
+            24,
+            background='#2e3440'
         ),
     ),
 ]
@@ -223,7 +155,6 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
-main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = False
 bring_front_click = False
 cursor_warp = False
@@ -239,6 +170,11 @@ floating_layout = layout.Floating(float_rules=[
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
@@ -250,7 +186,6 @@ focus_on_window_activation = "smart"
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
 
-# Autostart applications
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser('~/.config/qtile/autostart.sh')
